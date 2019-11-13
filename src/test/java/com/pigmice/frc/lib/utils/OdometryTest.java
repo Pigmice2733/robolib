a9044a54de939c7b24ae87795e017bf6db74b8ac
Brendan Burkhart <blackravenbrendan@gmail.com>
2019-11-12 23:23:52 -0800

    Upgrade gradlew to Gradle 6.0
    - Upgrade Gradle version
    - Remove unsupported Mockito features
    - Upgrade Hamcrest, JUnit, and Mockito deps

--- src/test/java/com/pigmice/frc/lib/utils/OdometryTest.java
+++ src/test/java/com/pigmice/frc/lib/utils/OdometryTest.java
@@ -1,28 +1,13 @@
 package com.pigmice.frc.lib.utils;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
-import com.pigmice.frc.lib.utils.Odometry.Streamer;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import edu.wpi.first.networktables.NetworkTableEntry;
 
 @ExtendWith(MockitoExtension.class)
 public class OdometryTest {
     private static final double epsilon = 1e-6;
-
-    NetworkTableEntry xEntry = mock(NetworkTableEntry.class);
-    NetworkTableEntry yEntry = mock(NetworkTableEntry.class);
-    NetworkTableEntry angleEntry = mock(NetworkTableEntry.class);
-
-    @InjectMocks
-    Odometry.Streamer streamer = new Streamer("/");
 
     @Test
     public void straightLineTest() {
@@ -92,22 +77,5 @@ public class OdometryTest {
         Assertions.assertEquals(50.0, odometer.getPosition().getX(), epsilon);
         Assertions.assertEquals(5.0, odometer.getPosition().getY(), epsilon);
         Assertions.assertEquals(-1.5 * Math.PI, odometer.getAngle(), epsilon);
-    }
-
-    @Test
-    public void streamTest() {
-        streamer.send(-20, 42, Math.PI);
-        streamer.send(-20, 40, Math.PI);
-        streamer.send(20, 40, Math.PI);
-        streamer.send(20, 40, -Math.PI);
-
-        verify(xEntry).setDouble(-20);
-        verify(xEntry).setDouble(20);
-
-        verify(yEntry).setDouble(42);
-        verify(yEntry).setDouble(40);
-
-        verify(angleEntry).setDouble(Math.PI);
-        verify(angleEntry).setDouble(-Math.PI);
     }
 }
