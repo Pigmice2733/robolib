d2f97fae09713c1aad29b538a8899049e2f144f5
Brendan Burkhart <blackravenbrendan@gmail.com>
2019-12-18 17:08:06 -0800

    Add addtional deadzone test

--- src/test/java/com/pigmice/frc/lib/controls/JoysticksTest.java
+++ src/test/java/com/pigmice/frc/lib/controls/JoysticksTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 public class JoysticksTest {
     @Test
     public void testDeadzone() {
+        Assertions.assertEquals(0, Joysticks.deadzone(0.0, 0.0));
         Assertions.assertEquals(0, Joysticks.deadzone(0.05, 0.1));
         Assertions.assertEquals(0, Joysticks.deadzone(-0.05, 0.1));
         Assertions.assertTrue(Utils.almostEquals(0.11111111, Joysticks.deadzone(0.2, 0.1)));
