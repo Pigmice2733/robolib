426f487c60f590a4b3c08a595ceecb85dd304e60
LucaHaverty <lucahaverty@gmail.com>
2023-06-02 11:13:25 -0700

    Path following commands

---
+++ src/main/java/com/pigmice/frc/lib/swerve/commands/path_following/DriveToPoint.java
@@ -0,0 +1,71 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.pigmice.frc.lib.swerve.commands.path_following;
+
+import java.util.List;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
+import com.pigmice.frc.lib.swerve.SwerveDrivetrain;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class DriveToPoint extends CommandBase {
+    private final SwerveDrivetrain drivetrain;
+    private final Pose2d targetPose;
+    private final XboxController controllerToRumble;
+
+    private Command pathCommand;
+
+    public DriveToPoint(SwerveDrivetrain drivetrain, Pose2d targetPose, XboxController controllerToRumble) {
+        this.drivetrain = drivetrain;
+        this.targetPose = targetPose;
+        this.controllerToRumble = controllerToRumble;
+    }
+
+    @Override
+    public void initialize() {
+        pathCommand = new SequentialCommandGroup(
+            new FollowPath(drivetrain, generateTrajectory(drivetrain, targetPose)),
+            new InstantCommand(() -> controllerToRumble.setRumble(RumbleType.kBothRumble, 0.75))
+        );
+        CommandScheduler.getInstance().schedule(pathCommand);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        if (pathCommand != null) {
+            pathCommand.cancel();
+        }
+        controllerToRumble.setRumble(RumbleType.kBothRumble, 0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return pathCommand.isFinished();
+    }
+
+    public static PathPlannerTrajectory generateTrajectory(SwerveDrivetrain drivetrain, Pose2d targetPose) {
+        Pose2d currentPose = drivetrain.getPose();
+
+        // Angle facing the end point when at the current point
+        Rotation2d angleToEnd = Rotation2d.fromRadians(Math.atan2(targetPose.getY() - currentPose.getY(),
+            targetPose.getX() - currentPose.getX()));
+        
+        PathPoint currentPoint = new PathPoint(currentPose.getTranslation(), angleToEnd, currentPose.getRotation());
+        PathPoint endPoint = new PathPoint(targetPose.getTranslation(), angleToEnd, targetPose.getRotation());
+        
+        return PathPlanner.generatePath(drivetrain.config.pathConstraints, List.of(currentPoint, endPoint));
+  }
+}
---
+++ src/main/java/com/pigmice/frc/lib/swerve/commands/path_following/FollowPath.java
@@ -0,0 +1,106 @@
+package com.pigmice.frc.lib.swerve.commands.path_following;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.FollowPathWithEvents;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+import com.pigmice.frc.lib.swerve.SwerveConfig;
+import com.pigmice.frc.lib.swerve.SwerveDrivetrain;
+
+import java.util.HashMap;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class FollowPath extends SequentialCommandGroup {
+
+    /**
+     * Use a SwerveController to follow a specified path.
+     * 
+     * @param drivetrain a drivetrain subsystem
+     * @param trajectory a path-following trajectory
+     */
+    public FollowPath(SwerveDrivetrain drivetrain, PathPlannerTrajectory trajectory) {
+        SwerveConfig config = drivetrain.config;
+        addCommands(
+            drivetrain.resetOdometryCommand(trajectory.getInitialHolonomicPose()),
+            new InstantCommand(() -> {SmartDashboard.putBoolean("Command Done", false);}),
+            new PPSwerveControllerCommand(
+                trajectory,
+                drivetrain::getPose,
+                config.kinematics,
+                config.pathDrivePID, // X controller
+                config.pathDrivePID, // Y controller 
+                config.pathTurnPID, // turn controller
+                (output) -> drivetrain.driveModuleStates(output), 
+                drivetrain
+            ),
+            new InstantCommand(() -> this.cancel())
+        );
+        addRequirements(drivetrain);
+    }
+
+    /**
+     * Use a SwerveController to follow a specified path.
+     * @param drivetrain a drivetrain subsystem
+     * @param trajectory a path-following trajectory
+     * @param eventMap commands to execute at certain events along the path (configure events in Path Planner)
+     */
+    public FollowPath(SwerveDrivetrain drivetrain, PathPlannerTrajectory trajectory, HashMap<String, Command> eventMap) {
+        SwerveConfig config = drivetrain.config;
+        addCommands(
+            drivetrain.resetOdometryCommand(trajectory.getInitialHolonomicPose()),
+            new FollowPathWithEvents(
+                new PPSwerveControllerCommand(
+                    trajectory,
+                    drivetrain::getPose,
+                    config.kinematics,
+                    config.pathDrivePID, // X controller
+                    config.pathDrivePID, // Y controller 
+                    config.pathTurnPID, // turn controller
+                    (output) -> drivetrain.driveModuleStates(output), 
+                    drivetrain
+                ),
+                trajectory.getMarkers(),
+                eventMap
+            )
+        );
+        addRequirements(drivetrain);
+    }
+
+    /**
+     * Use a SwerveController to follow a specified path.
+     * 
+     * @param drivetrain a drivetrain subsystem
+     * @param pathName the name of a premade path to follow
+     * @param reversed reverse the robots direction
+     */
+    public FollowPath(SwerveDrivetrain drivetrain, String pathName, boolean reversed) {
+        this(
+                drivetrain,
+                PathPlanner.loadPath(
+                        pathName,
+                        drivetrain.config.pathConstraints,
+                        reversed));
+    }
+
+    /**
+     * Use a SwerveController to follow a specified path.
+     * 
+     * @param drivetrain a drivetrain subsystem
+     * @param pathName the name of a premade path to follow
+     * @param eventMap commands to execute at certain events along the path (configure events in Path Planner)
+     * @param reversed reverse the robots direction
+     */
+    public FollowPath(SwerveDrivetrain drivetrain, String pathName, HashMap<String, Command> eventMap, boolean reversed) {
+        this(
+                drivetrain,
+                PathPlanner.loadPath(
+                        pathName,
+                        drivetrain.config.pathConstraints,
+                        reversed),
+                eventMap);
+    }
+}
